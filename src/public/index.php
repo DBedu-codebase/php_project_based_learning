928c4851a7681f7c67e70a387bc92d9158bea6d0
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-12-04 10:28:52 +0700

    feat: create boilerplate bab B

---
+++ src/public/index.php
@@ -0,0 +1,12 @@
+<?php
+// ! Don't remove the line below
+$currentYear = getdate()['year'];
+// ? Variable Declaration, write your code here
+
+// * Result Output & echo your variable here
+echo "Name: " . PHP_EOL;
+echo "Age: " . PHP_EOL;
+echo "Birth Year: " . PHP_EOL;
+echo "Status: " . PHP_EOL;
+echo "Hobi: " . PHP_EOL;
+echo "Is teenager?: " . PHP_EOL;
